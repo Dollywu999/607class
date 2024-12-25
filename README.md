fb0fd9f5a8724bc5796d365b1b28294f9fda9465
Dollywu999 <wudolly0926@gmail.com>
2024-12-25 20:36:00 +0800

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# 607class
+# Untitled
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/Dolly-Wu-the-styleful/pen/KwPqbVZ](https://codepen.io/Dolly-Wu-the-styleful/pen/KwPqbVZ).
+
---
+++ script.js
@@ -0,0 +1,12 @@
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-app.js"></script>
+<script src="https://www.gstatic.com/firebasejs/9.6.1/firebase-firestore.js"></script>
+const firebaseConfig = {
+    apiKey: "AIzaSyB_r4w3pb1ZIO8YUCA9Yl9IITSHZi3cCiI",
+    authDomain: "class-6bb0b.firebaseapp.com",
+    projectId: "class-6bb0b",
+    storageBucket: "class-6bb0b.appspot.com",
+    messagingSenderId: "709913685558",
+    appId: "1:709913685558:web:925486cd1b5b9f99527297",
+};
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
